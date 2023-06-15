bd5fa84d0f70564f224e31196f36a74afd41132c
usmanmaqbool <usmanmaqbool@outlook.com>
2023-06-15 15:02:42 +0800

    running-but-not-giving-results

--- test_model.sh
+++ test_model.sh
@@ -5,7 +5,7 @@ PYTHON=${PYTHON:-"python3"}
 
 DATASET=tokyo
 
-BASEDir="/media/leo/2C737A9872F69ECF/models/graphnetvlad/pytorch-netvlad/pittsburgh-sare_ind-lr0.001-05-Jun-2239/checkpoints/"
+BASEDir="/media/leo/2C737A9872F69ECF/models/openibl/official-sare/conv5-sare_ind-lr0.001-tuple4/"
 FILES="${BASEDir}*.tar"
 echo "${FILES}"
 
@@ -13,7 +13,6 @@ for RESUME in $FILES
 do
   echo "Building PCA Model of $RESUME file..."
   # take action on each file. $f store current file name
-
   
   python add_pca.py \
   --config_path patchnetvlad/configs/train.ini \
@@ -36,14 +35,15 @@ do
   --output_features_dir=/media/leo/2C737A9872F69ECF/why-so-deepv2-data/pittsburgh/patch-netvlad-features/pitts30k_index \
   --resume_path=${PCA_RESUME}
 
-  exit
+  
   echo "Extracting Features of Query Images"
 
   python feature_extract.py \
   --config_path patchnetvlad/configs/performance.ini \
   --dataset_file_path=pitts30k_imageNames_query.txt \
   --dataset_root_dir=/media/leo/2C737A9872F69ECF/datasets/maqbool-datasets/datasets-place-recognition/Test_Pitts250k/ \
-  --output_features_dir=/media/leo/2C737A9872F69ECF/why-so-deepv2-data/pittsburgh/patch-netvlad-features/pitts30k_query 
+  --output_features_dir=/media/leo/2C737A9872F69ECF/why-so-deepv2-data/pittsburgh/patch-netvlad-features/pitts30k_query \
+  --resume_path=${PCA_RESUME}
   echo "Performing Features Matching and Recall Result"
 
   python feature_match.py \

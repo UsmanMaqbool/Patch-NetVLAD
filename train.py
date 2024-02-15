0ea21ad21ad6b24d85239aa797b50661803f5f8e
usmanmaqbool <usmanmaqbool@outlook.com>
2024-02-15 12:19:06 -0500

    bugs-fix

--- train.py
+++ train.py
@@ -161,8 +161,9 @@ if __name__ == "__main__":
                                       'num_clusters'] + '_desc_cen.hdf5')
         if opt.cluster_path:
             if isfile(opt.cluster_path):
-                shutil.copyfile(opt.cluster_path, initcache)
-                print("===> Loading Cluster Centroids from '{}'".format(opt.cluster_path))
+                if opt.cluster_path != initcache:
+                    shutil.copyfile(opt.cluster_path, initcache)
+                    print("===> Loading Cluster Centroids from '{}'".format(opt.cluster_path))
             else:
                 raise FileNotFoundError("=> no cluster data found at '{}'".format(opt.cluster_path))
         else:

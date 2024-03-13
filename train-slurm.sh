9b429e37730714d425e8a7bffc8120f6113163c9
Usman Maqbool <usmanmaqbool@outlook.com>
2024-03-13 07:38:58 -0400

    make-it-4-gpus

--- train-slurm.sh
+++ train-slurm.sh
@@ -13,9 +13,9 @@
 #SBATCH --output=R-%x.%j.out
 #SBATCH --error=R-%x.%j.err
 #SBATCH --nodes=1 
-#SBATCH --gpus-per-node=a100:8   
+#SBATCH --gpus-per-node=a100:4   
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48    # There are 24 CPU cores on P100 Cedar GPU nodes
+#SBATCH --cpus-per-task=32    # There are 24 CPU cores on P100 Cedar GPU nodes
 #SBATCH --constraint=a100
 #SBATCH --mem-per-cpu=4gb
 

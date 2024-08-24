2437c6df87aa6ab715f8465bcb4396a2df18c92f
Usman Maqbool <usmanmaqbool@outlook.com>
2024-08-24 07:38:17 -0400

    0824-s-1

--- train-slurm.sh
+++ train-slurm.sh
@@ -17,7 +17,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40    # There are 24 CPU cores on P100 Cedar GPU nodes
 #SBATCH --constraint=a100
-#SBATCH --mem-per-cpu=8GB
+#SBATCH --mem-per-cpu=16GB
 #SBATCH --distribution=cyclic:cyclic
 
 ## To RUN
